75dbcc931729d78b2f8eab82a7204554fe12ab44
Åke Engelbrektson <eson57@users.noreply.github.com>
2026-05-17 11:21:42 +0200

    Create harbour-openhab-sv.ts
    Add Swedish translation
    
    Signed-off-by: Åke Engelbrektson <eson57@users.noreply.github.com>

---
+++ translations/harbour-openhab-sv.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>LegalPage</name>
+    <message>
+        <source>Legal</source>
+        <translation>Juridik</translation>
+    </message>
+    <message>
+        <source>Copyright © 2026, Contributors to the openHAB project</source>
+        <translation>Copyright © 2026, Bidragsgivare till openHAB-projektet</translation>
+    </message>
+</context>
+<context>
+    <name>MainUiPage</name>
+    <message>
+        <source>openHAB Main UI</source>
+        <translation>openHAB Huvudgränssnitt</translation>
+    </message>
+</context>
+<context>
+    <name>PrivacyPolicyPage</name>
+    <message>
+        <source>Privacy Policy</source>
+        <translation>Integritetspolicy</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Allmänt</translation>
+    </message>
+    <message>
+        <source>Demo Mode</source>
+        <translation>Demoläge</translation>
+    </message>
+    <message>
+        <source>If selected, Demo SiteMaps and DemoPages will be shown.</source>
+        <translation>Om markerat kommer Demo-sidkartor och Demo-sidor att visas.</translation>
+    </message>
+    <message>
+        <source>Local server</source>
+        <translation>Lokal server</translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <source>https://demo.openhab.org</source>
+        <translation>https://demo.openhab.org</translation>
+    </message>
+    <message>
+        <source>Cover actions</source>
+        <translation>Miniatyråtgärder</translation>
+    </message>
+    <message>
+        <source>Configure Item-IDs and commands for the app cover quick actions. Leave empty to hide an action.</source>
+        <translation>Konfigurera artikel-ID:n och kommandon för appens miniatyr, för snabba åtgärder. Lämnas tomt för att dölja en åtgärd.</translation>
+    </message>
+    <message>
+        <source>Left button</source>
+        <translation>Vänster knapp</translation>
+    </message>
+    <message>
+        <source>Item-ID</source>
+        <translation>Artikel-ID</translation>
+    </message>
+    <message>
+        <source>Command</source>
+        <translation>Kommando</translation>
+    </message>
+    <message>
+        <source>e.g. ON, OFF, TOGGLE</source>
+        <translation>T.ex. PÅ, AV, VÄXLA</translation>
+    </message>
+    <message>
+        <source>Right button</source>
+        <translation>Höger knapp</translation>
+    </message>
+    <message>
+        <source>Scroll to top</source>
+        <translation>Skrolla till toppen</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>App Version</source>
+        <translation>Appversion</translation>
+    </message>
+    <message>
+        <source>Legal</source>
+        <translation>Juridik</translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation>Integritetspolicy</translation>
+    </message>
+    <message>
+        <source>e.g. item_id1</source>
+        <translation>T.ex. artikel_id1</translation>
+    </message>
+    <message>
+        <source>Report a bug or request a feature</source>
+        <translation>Rapportera ett fel eller be om en funktion</translation>
+    </message>
+</context>
+<context>
+    <name>SitemapPage</name>
+    <message>
+        <source>Scroll to top</source>
+        <translation>Skrolla till toppen</translation>
+    </message>
+</context>
+<context>
+    <name>SitemapPullDownMenu</name>
+    <message>
+        <source>Refresh Sitemaps</source>
+        <translation>Uppdatera sidkartor</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>─────────────────</source>
+        <translation>─────────────────</translation>
+    </message>
+    <message>
+        <source>Main</source>
+        <translation>Viktigast</translation>
+    </message>
+</context>
+<context>
+    <name>SitemapSelectionPage</name>
+    <message>
+        <source>Navigation</source>
+        <translation>Navigering</translation>
+    </message>
+    <message>
+        <source>Sitemaps</source>
+        <translation>Sidkartor</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Refresh Sitemaps</source>
+        <translation>Uppdatera sidkartor</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Hem</translation>
+    </message>
+    <message>
+        <source>Main</source>
+        <translation>Viktigast</translation>
+    </message>
+    <message>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+</context>
+</TS>
